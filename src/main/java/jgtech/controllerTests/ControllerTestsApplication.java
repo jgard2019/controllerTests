ad8ec957cc905f0fad16786633c270212445888b
jgard2019 <jwgardner22@gmail.com>
2022-02-28 11:56:32 -0500

    initial commit

---
+++ src/main/java/jgtech/controllerTests/ControllerTestsApplication.java
@@ -0,0 +1,13 @@
+package jgtech.controllerTests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllerTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControllerTestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jgtech/controllerTests/ControllerTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package jgtech.controllerTests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllerTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
